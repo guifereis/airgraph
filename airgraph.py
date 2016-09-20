fdcca8ac99f3733305ce6c7a3cae4ded3e6f721a
Guilherme Reis <guifereis@gmail.com>
2016-09-20 04:51:17 +0100

    Fixed argument bug.

--- airgraph.py
+++ airgraph.py
@@ -45,7 +45,8 @@ def addNeighborsToReachable(reachable, nextReachable): # for all in reachable, a
 reachable = set() # IDs reachable in 1 hop
 nextReachable = set() # IDs reachable in 2 hops
 
-getOutNeigh(gg.GetNI(airportToID["SIN"]), reachable)
+getOutNeigh(gg.GetNI(airportToID[srcPort]), reachable)
+#getOutNeigh(gg.GetNI(airportToID["SIN"]), reachable)
 #getOutNeigh(gg.GetNI(airportToID["DXB"]), reachable)
 
 addNeighborsToReachable(reachable, nextReachable)
